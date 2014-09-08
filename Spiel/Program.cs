90c65979188b29a70df8aaf3856953453e30e792
Haschiwu <christoph.hollmann@yahoo.de>
2014-09-08 16:50:19 +0200

    nu

---
+++ Spiel/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// EINBINDEN VON SFML.NET
+//  - Projektmappen-Explorer öffnen
+//  - Rechtklick -> Verweis hinzufügen
+//  - In der Linkenspalte "Durchsuchen"
+//  - Ganz unten erneut auf "Durchsuchen"
+//  - SFML.NET Ordner öffnen -> Libs
+//  - Alle Auswählen und hinzufügen
+//  - Schauen ob alle markiert sind und OK
+//
+//  - Rechtsklick -> Hinzufügen -> Vorhandenes Element
+//  - SFML.NET Ordner öffnen -> extlibs
+//  - ggf. "Alle Dateitypen (.*)" auswählen
+//  - Alle Auswählen und hinzufügen
+//  - Im Projektmappen-Explorer die 5 .dll auswählen
+//  - Rechtsklick -> Eigenschaften
+//  - Ins Ausgabeverzeichniskopieren -> "Immer kopieren" oder "Kopieren wenn neuer"
+
+// KONSOLE AUSSCHALTEN
+//  - Projektmappen-Explorer öffnen
+//  - Rechtsklick auf das Projekt (Intro2D-02-Beispiel) -> Eigenschaften
+//  - In den Reiter "Anwendung" (automatisch offen) wechseln
+//  - Ausgabetyp -> "Windows-Anwendung"
+
+// WICHTIG !!!!!!
+//  - WENN IHR DIESES PROJEKT WEITERVERWENDEN WOLLT, MÜSST IHR DIE VERWEISE (erster Teil) NEU HINZUFÜGEN
+
+// Wird für Programm ablauf benötigt
+
+namespace Spiel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
